fb166e31dbd800c7ad0208e80e150502a680a7ea
KarlA <Karl.adamson@tktk.ee>
2024-03-15 13:58:14 +0200

    add week6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = {"mysushi", "pizzakiosk", "rääguburger", "doner kebab", "chopsticks nr. 17"};
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Täna me sööme {snacks[randomIndex]}");
